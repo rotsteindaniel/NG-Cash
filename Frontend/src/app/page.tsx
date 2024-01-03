83c1482fce20cfecc3204bcd33739a63a3217522
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2024-01-03 19:27:41 -0300

    Fix formatting issues in code

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import styles from "./page.module.css";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Form
+        name="normal_login"
+        // className="login-form"
+        className={styles.loginForm}
+        initialValues={{ remember: true }}
+      >
+        <h1 className={styles.title}>Login</h1> <br />
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>{" "}
+          <br />
+          <br />
+          Or <Link href="/register">register now!</Link>
+        </Form.Item>
+      </Form>
+    </main>
+  );
+}
---
+++ Frontend/src/app/register/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import styles from "./page.module.css";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+import Link from "next/link";
+
+export default function Register() {
+  return (
+    <main className={styles.main}>
+      <Form
+        name="normal_login"
+        // className="login-form"
+        className={styles.registerForm}
+        initialValues={{ remember: true }}
+      >
+        <h1 className={styles.title}>Register</h1> <br />
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Register
+          </Button>{" "}
+          <br />
+          <br />
+          Or <Link href="/">Go to login page!</Link>
+        </Form.Item>
+      </Form>
+    </main>
+  );
+}
