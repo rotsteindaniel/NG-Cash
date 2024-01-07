cb87ffa0ed9e20ed071dfc85c5c7b6f6853822a2
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2024-01-07 12:10:52 -0300

    Update MainPage component

--- Frontend/src/app/(private)/mainpage/page.tsx
+++ Frontend/src/app/(private)/mainpage/page.tsx
@@ -1,14 +1,32 @@
 "use client";
 import Link from "next/link";
 import styles from "./page.module.css";
-import { Button, Card, Flex, Space, Typography } from "antd";
-import { useContext } from "react";
+import { Button, Card, Flex, Space, Typography, message } from "antd";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
+import { useRouter } from "next/navigation";
 
 const { Title } = Typography;
 
 export default function MainPage() {
-  const { logOut } = useContext(AuthContext);
+  const Router = useRouter();
+
+  const { user, setUser, isAuthenticated, recoverUserInformation, logOut } =
+    useContext(AuthContext);
+
+  async function fetchUserData() {
+    const userData = await recoverUserInformation();
+    setUser(userData?.user || null);
+  }
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      Router.push("/");
+      message.error("You are not logged in.");
+    } else {
+      fetchUserData();
+    }
+  }, [isAuthenticated]);
 
   return (
     // <main className={styles.main}>
@@ -20,7 +38,7 @@ export default function MainPage() {
           {/* </Link> */}
         </Button>
         <Flex vertical align="center">
-          <Title level={2}>Oi user Daniel</Title>
+          <Title level={2}>Olá</Title>
         </Flex>
         <Flex vertical align="center">
           <Title level={4}>Balance: R$ 100,00</Title>
